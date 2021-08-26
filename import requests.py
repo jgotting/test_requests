c32c27c89e3356e21e93ff90f4be6aacebd2d942
jgotting <johan@gotting.com>
2021-08-26 14:09:58 +0200

    Updated comments

--- import requests.py
+++ import requests.py
@@ -1,8 +1,8 @@
 import requests
 response = requests.get("https://jsonplaceholder.typicode.com/todos/2")
 print(response.status_code)
-res = (response.json()) #Transform JSON to a python dictionary
+res = (response.json())  # Transform JSON to a python dictionary
 print(res)
-print(type(res)) #Check the type
-for l in res: #Iterate through the Dict
-    print(l, " --> ", res[l])
+print(type(res))  # Check the type
+for key in res:  # Iterate through the Dict
+    print(key, " --> ", res[key])
